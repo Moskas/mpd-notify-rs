bfa5380ee606f70698b4c9a050345b3ec6afb326
Moskas <minemoskas@gmail.com>
2023-03-03 17:20:23 +0100

    Added mpd status handling, fixed panic on launch if queue is empty.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod config;
 mod notification;
 mod song_info;
+use mpd::{Client, State};
 use std::thread::sleep;
 use std::time;
 
@@ -8,13 +9,41 @@ use notification::send_notification;
 use song_info::get_metadata;
 
 fn main() {
-    let mut current_song: Vec<String> = get_metadata();
+    let config = config::get_config();
+    let mut connection = Client::connect(&format!("{}:{}", config.ip, config.port)[..]).unwrap();
+    let mut current_song = String::new();
+    let mut current_status = State::Pause;
     loop {
-        if get_metadata() != current_song {
-            send_notification(get_metadata());
-            current_song = get_metadata();
-        };
-        //println!("{}", current_song[3]);
-        sleep(time::Duration::from_millis(500));
+        if connection.currentsong().unwrap() != None {
+            if connection.status().unwrap().state != current_status
+                || get_metadata()[1] != current_song
+            {
+                current_song = connection.currentsong().unwrap().unwrap().title.unwrap();
+                match connection
+                    .status()
+                    .expect("Can not get current playback state")
+                    .state
+                {
+                    State::Play => {
+                        current_status = State::Play;
+                        //current_song = connection.currentsong().unwrap().unwrap().title.unwrap();
+                        send_notification(get_metadata(), State::Play);
+                    }
+                    State::Stop => {
+                        current_status = State::Stop;
+                        //println!("Playback stopped");
+                        send_notification(get_metadata(), State::Stop);
+                    }
+                    State::Pause => {
+                        current_status = State::Pause;
+                        //println!("Playback paused");
+                        send_notification(get_metadata(), State::Pause);
+                    }
+                };
+            };
+            sleep(time::Duration::from_millis(500));
+        } else {
+            sleep(time::Duration::from_millis(500));
+        }
     }
 }
--- src/notification.rs
+++ src/notification.rs
@@ -1,17 +1,38 @@
+use mpd::State;
 use notify_rust::Notification;
 
 #[cfg(not(target_os = "macos"))]
-pub fn send_notification(metadata: Vec<String>) {
-    Notification::new()
-        .summary("Now playing:")
-        .body(&format!("{}\n{}\nfrom {}", metadata[0], metadata[1], metadata[2])[..])
-        .timeout(5000)
-        .image_path(&format!("{}", metadata[3])[..]) // finding cover image path WIP
-        .show()
-        .unwrap();
+pub fn send_notification(metadata: Vec<String>, state: State) {
+    match state {
+        State::Play => {
+            Notification::new()
+                .summary("Now playing:")
+                .body(&format!("{}\n{}\nfrom {}", metadata[0], metadata[1], metadata[2])[..])
+                .timeout(5000)
+                .image_path(&format!("{}", metadata[3])[..]) // finding cover image path WIP
+                .show()
+                .unwrap();
+        }
+        State::Pause => {
+            Notification::new()
+                .summary("MPD status")
+                .body("Playback paused")
+                .timeout(1000)
+                .show()
+                .unwrap();
+        }
+        State::Stop => {
+            Notification::new()
+                .summary("MPD status")
+                .body("Playback stopped")
+                .timeout(1000)
+                .show()
+                .unwrap();
+        }
+    }
 }
 #[cfg(target_os = "macos")]
-pub fn send_notification(metadata: Vec<String>) {
+pub fn send_notification(metadata: Vec<String>, state: mpd::State) {
     Notification::new()
         .summary("Now playing:")
         .body(&format!("{}\n{}\nfrom {}", metadata[0], metadata[1], metadata[2])[..])
